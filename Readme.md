1fe6d0a1d929afbac0e2d2cad791aa3d208f285d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Report Designer for WPF - How to enable end-users to load custom assemblies to the Entity Framework context
+
+
+In the <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument114851">Entity Framework Data Source wizard</a>, it is possible to load custom assemblies by using the Browse button on the <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument114856">Select the Data Context</a> page.<br><img src="https://raw.githubusercontent.com/DevExpress-Examples/report-designer-for-wpf-how-to-enable-end-users-to-load-custom-assemblies-to-the-entity-fr-t503673/17.1.3+/media/4b00efec-2108-11e7-80bf-00155d62480c.png"><br>In the End-User Designer, this button is hidden by default, so that end-users are allowed only to select the data context from assemblies referenced by the project.<br><img src="https://raw.githubusercontent.com/DevExpress-Examples/report-designer-for-wpf-how-to-enable-end-users-to-load-custom-assemblies-to-the-entity-fr-t503673/17.1.3+/media/5cb8b6d5-2108-11e7-80bf-00155d62480c.png"><br>This example illustrates how to enable this functionality in the End-User Report Designer for WPF.<br>To learn more, see <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument117318">Security Considerations</a>.
+
+<br/>
+
+
---
+++ VB/EnableEFBrowseButton/UseEFOptionsAndCustomization.xaml.vb
@@ -0,0 +1,40 @@
+﻿Imports DevExpress.DataAccess.Wizard
+Imports DevExpress.DataAccess.Wizard.Model
+Imports DevExpress.Xpf.DataAccess.DataSourceWizard
+Imports DevExpress.Xpf.Reports.UserDesigner.ReportWizard
+Imports System.Windows.Controls
+
+Namespace EnableEFBrowseButton
+    ''' <summary>
+    ''' Interaction logic for UseEFOptionsAndCustomization.xaml
+    ''' </summary>
+    Partial Public Class UseEFOptionsAndCustomization
+        Inherits UserControl
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+
+    Public Class WizardCustomizationService
+        Implements IWizardCustomizationService
+
+        Public Sub CustomizeDataSourceWizard(ByVal customization As DataSourceWizardCustomizationModel, ByVal container As ViewModelSourceIntegrityContainer) Implements IWizardCustomizationService.CustomizeDataSourceWizard
+            Dim options = container.Resolve(Of EFWizardOptions)() Or EFWizardOptions.ShowBrowseButton
+            container.RegisterInstance(options)
+        End Sub
+        Public Sub CustomizeReportWizard(ByVal customization As ReportWizardCustomizationModel, ByVal container As ViewModelSourceIntegrityContainer) Implements IWizardCustomizationService.CustomizeReportWizard
+            Dim options = container.Resolve(Of EFWizardOptions)() Or EFWizardOptions.ShowBrowseButton
+            container.RegisterInstance(options)
+        End Sub
+        Private Function IDataSourceWizardCustomizationService_TryCreateDataSource(ByVal model As IDataSourceModel, ByRef dataSource As Object, ByRef dataMember As String) As Boolean Implements IDataSourceWizardCustomizationService.TryCreateDataSource
+            dataSource = Nothing
+            dataMember = Nothing
+            Return False
+        End Function
+        Private Function IWizardCustomizationService_TryCreateReport(ByVal model As DevExpress.XtraReports.Wizards.XtraReportModel, ByRef report As DevExpress.XtraReports.UI.XtraReport) As Boolean Implements IWizardCustomizationService.TryCreateReport
+            report = Nothing
+            Return False
+        End Function
+    End Class
+End Namespace
